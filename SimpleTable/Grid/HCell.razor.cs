73dd669ba36d68afd702c0aefad0b339cb52a5ad
Rahul Chakrabarty <rahulchrty@gmail.com>
2024-02-28 19:36:33 +0530

    Change component names

---
+++ SimpleTable/Grid/HCell.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTable.Grid
+{
+    public partial class HCell
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
